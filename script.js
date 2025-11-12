267be06b2d8f3cf69c7a361abcf7fdc0be131dca
keerui <178910386+krsng-lab@users.noreply.github.com>
2025-11-12 11:06:43 +0800

    Change hard-coded values to variables

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 256; i++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("gridSquare");
-    gridSquare.style.width = "37.5px";
-    gridSquare.style.height = "37.5px";
-    gridSquare.addEventListener('mouseenter', function() {
-        gridSquare.style.backgroundColor = "blue";
-    })
-    container.appendChild(gridSquare);
-} 
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function() {
     let gridSize = prompt("Enter grid size (max 100)");
     if (gridSize > 100) {
     alert("Please enter a valid grid size");
-    }}
-);
+    }
+    
+    for (let i = 0; i < gridSize ** 2; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    gridSquare.style.width = `${600/gridSize}px`;
+    gridSquare.style.height = `${600/gridSize}px`;
+    gridSquare.addEventListener('mouseenter', function() {
+        gridSquare.style.backgroundColor = "blue";
+    })
+    container.appendChild(gridSquare);
+} 
+
+});
 
 /* 
 The goal is to make the squares divs appear as grids in the 
