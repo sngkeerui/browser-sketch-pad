8768d3eae1853abc1d1a233bb9d3ff6bf4c83f50
keerui <178910386+krsng-lab@users.noreply.github.com>
2025-11-13 09:12:12 +0800

    Add condition to reject grid size values below 1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector("#container");
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function() {
     let gridSize = prompt("Enter grid size (max 100)");
-    if (gridSize > 100) {
+    if (gridSize <= 0 || gridSize > 100) {
     alert("Please enter a valid grid size");
     return;
     }
@@ -24,8 +24,7 @@ btn.addEventListener("click", function() {
     gridSquare.style.height = `${600/gridSize}px`;
     gridSquare.addEventListener('mouseenter', () => gridSquare.style.backgroundColor = getRandomColor());
     container.appendChild(gridSquare);
-} 
-});
+}});
 
 /* 
 The goal is to make the squares divs appear as grids in the 
