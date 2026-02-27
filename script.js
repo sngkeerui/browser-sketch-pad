85d4f883c48ec81052a646a136f62c27b42793d6
Kee Rui <178910386+krsng-lab@users.noreply.github.com>
2026-02-27 11:47:44 +0800

    feat: add dynamic grid generation

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ const container = document.getElementById("container");
 const create = document.getElementById("create");
 
 create.addEventListener("click", () => {
-    const size = +prompt("Enter grid size (1-100)");
-    if (size < 1 || size > 100 || isNaN(size)) {
-        return alert("Please enter a valid grid size!");
+    const gridSize = +prompt("Enter grid size (1-100)");
+    if (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
+        alert("Please enter a valid grid size!");
+        return;
     }
-})
-    
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("gridSquare");
+        gridSquare.style.flex = `0 0 ${100 / gridSize}%`;
+        gridSquare.style.height = `${100 / gridSize}%`;
+        container.appendChild(gridSquare);
+}})
